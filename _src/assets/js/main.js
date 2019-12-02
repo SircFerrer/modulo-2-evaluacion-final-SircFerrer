4333331e1de5d8f456dba39217191e0c3f027d40
sircferrer <ferrercarballo@gmail.com>
2019-12-02 11:35:56 +0100

    add name anmg image with js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,6 +2,7 @@
 
 const input = document.querySelector(".js-input");
 const button = document.querySelector(".js-button");
+const container = document.querySelector(".js-container");
 
 let series = [];
 
@@ -15,10 +16,31 @@ function getServerData(event) {
     .then(function(serverData) {
       series = serverData;
 
-      console.log(series);
+      paintSeries();
+
+      //   console.log(series);
     })
     .catch(function(err) {
       console.log("Error al traer los datos del servidor", err);
     });
+}
+
+function paintSeries() {
+  console.log(series);
+  let htmlCode = "";
+  for (const serie of series) {
+    htmlCode += "<li class='container__list__item'>";
+    debugger;
+    if (serie.show.image === null) {
+      htmlCode +=
+        '<img class="item__img"src="./assets/images/tvPlaceholder.png"/>';
+    } else {
+      htmlCode += `<img class='item__img'src='${serie.show.image.medium}'/>`;
+    }
+    htmlCode += `<h3>${serie.show.name}</h3>`;
+
+    htmlCode += "</li>";
+  }
+  container.innerHTML = htmlCode;
 }
 button.addEventListener("click", getServerData);
