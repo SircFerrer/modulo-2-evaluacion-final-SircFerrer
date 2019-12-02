55d916181feb4fdbf53591568f03e44daf48eea0
sircferrer <ferrercarballo@gmail.com>
2019-12-02 10:11:50 +0100

    add html and fetch to js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,24 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const input = document.querySelector(".js-input");
+const button = document.querySelector(".js-button");
+
+let series = [];
+
+function getServerData(event) {
+  let userInput = input.value;
+  event.preventDefault();
+  fetch(`http://api.tvmaze.com/search/shows?q=${userInput}`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(serverData) {
+      series = serverData;
+
+      console.log(series);
+    })
+    .catch(function(err) {
+      console.log("Error al traer los datos del servidor", err);
+    });
+}
+button.addEventListener("click", getServerData);
--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,1 +1,4 @@
-"use strict";console.log(">> Ready :)");
+"use strict";
+
+"use strict";
+console.log(">> Ready :)");
